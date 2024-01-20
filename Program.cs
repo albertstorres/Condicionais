13a6c5df103a048fddcf81fb392e2bde9cf4d45c
Albert Sevy <albertstorres@gmail.com>
2024-01-19 22:00:26 -0300

    Repositório para estudo das condicionais.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONDICIONAIS IF E ELSE
+            int idade = 18;
+            int maiorIdade = 21;
+
+            if (idade >= maiorIdade)
+                Console.WriteLine("É maior de idade.");
+            else
+                Console.WriteLine("Não é maior de idade.");
+            //FIM CONDICIONAIS IF E ELSE.
+
+            //CONDICIONAIS SWITCH
+
+            //FIM CONCIONAIS SWITCH
+        }
+    }
+}
